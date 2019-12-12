a7125808d58054a248c08932b9c83b6805c7f55c
Meditech <visitamt@gmail.com>
2019-12-12 14:43:06 +0100

    Add these to previous commit

---
+++ server/db/mongoose_connect_local_or_heroku.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+//create an instancew of mongoose that connect to a new/ existing Db [codingClass]
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/allpurpose', {useNewUrlParser: true});
+
+
+module.exports = {
+  mongoose // ES6 Since the properties and the variables have thesame name
+};
